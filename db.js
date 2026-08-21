5c8412dbd3826c81ed64512f6584580eacd4929e
Md. Rokanuzzaman <roka.birganj@gmail.com>
2026-08-22 01:49:10 +0600

    fix serverless error

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
 
 if (!supabaseUrl || !supabaseKey) {
-    console.error("Missing Supabase URL or Key in .env file!");
+    console.error("Missing Supabase URL or Key!");
 }
 
 const supabase = createClient(supabaseUrl, supabaseKey);
